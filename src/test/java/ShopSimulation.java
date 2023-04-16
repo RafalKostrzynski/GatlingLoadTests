6a0d2923cb32b8f871a452cca1c8dcb90080d8eb
RafalKostrzynski <kostrzynskirafal@wp.pl>
2023-04-16 13:04:45 +0200

    Introduced silent Warmup phase

--- src/test/java/ShopSimulation.java
+++ src/test/java/ShopSimulation.java
@@ -2,10 +2,7 @@ import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import java.lang.reflect.Array;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
 import static io.gatling.javaapi.core.CoreDsl.bodyString;
@@ -18,12 +15,17 @@ public class ShopSimulation extends Simulation {
 
     public ShopSimulation() {
 
-        final var populationBuilder = authenticationScenario.injectOpen(
-                        constantUsersPerSec(100).during(Duration.ofSeconds(1))
-                )
-                .protocols(httpProtocol);
+        final var warmupTest = warmupScenario.injectOpen(
+                constantUsersPerSec(50).during(Duration.ofMinutes(5))
+        ).protocols(httpProtocol.silentUri("https://localhost:8080/*"));
 
-        this.setUp(List.of(populationBuilder));
+        final var stressTest = testScenario.injectOpen(
+                constantUsersPerSec(500).during(Duration.ofSeconds(30))
+        ).protocols(httpProtocol);
+
+        final var shopTest = warmupTest.andThen(stressTest);
+
+        this.setUp(shopTest);
     }
 
     HttpProtocolBuilder httpProtocol = http
@@ -34,7 +36,7 @@ public class ShopSimulation extends Simulation {
             .acceptEncodingHeader("gzip, deflate")
             .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 
-    ScenarioBuilder authenticationScenario = scenario("Authentication")
+    ScenarioBuilder testScenario = scenario("Stress test")
             .feed(Utils.userFeeder)
             .feed(Utils.productFeeder, 3)
             .feed(Utils.courierFeeder)
@@ -83,4 +85,47 @@ public class ShopSimulation extends Simulation {
                             )
             );
 
+    ScenarioBuilder warmupScenario = scenario("Warmup")
+            .feed(Utils.userFeeder)
+            .feed(Utils.productFeeder, 3)
+            .feed(Utils.courierFeeder)
+            .feed(Utils.regionFeeder)
+            .exec(http("Authenticate")
+                    .post("/v1/api/auth/authenticate")
+                    .silent()
+                    .body(StringBody("""
+                            {
+                                "username": "#{user}",
+                                "password": "#{user}"
+                            }
+                            """))
+                    .asJson()
+                    .ignoreProtocolChecks()
+                    .check(bodyString().saveAs("token"))
+            )
+            .exec(
+                    http("Silent Calculate order details")
+                            .get("/v1/api/order-details")
+                            .silent()
+                            .header("Authorization", "Bearer #{token}")
+                            .body(StringBody("""
+                                    {
+                                        "courier":"#{courier}",
+                                        "region":"#{region}",
+                                        "products":["#{product(0)}", "#{product(1)}", "#{product(2)}"]
+                                    }
+                                    """)
+                            )
+                            .asJson()
+                            .check(bodyString().saveAs("order"))
+            )
+            .exec(
+                    http("Silent Place order")
+                            .post("/v1/api/order")
+                            .silent()
+                            .header("Authorization", "Bearer #{token}")
+                            .body(StringBody("#{order}"))
+                            .asJson()
+                            .ignoreProtocolChecks()
+            );
 }
